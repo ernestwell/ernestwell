5ccf55588e2b155485d9e3f381d9ff25bfafcd4b
Bhavatharani <bhavatharni.j@gmail.com>
2025-05-02 18:28:00 +0530

    routed

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,8 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/ernestwell/", // 👈 Add this if deploying to a subdirectory
   plugins: [react()],
   build: {
-    outDir: "dist", // Ensure Vercel picks up this folder
+    outDir: "dist",
   },
 });
