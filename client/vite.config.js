e3a633c707b981354fd90d911854a8d1c43503e8
Bhavatharani <bhavatharni.j@gmail.com>
2025-05-03 01:16:58 +0530

    updated1

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/ernestwell/", // 👈 Add this if deploying to a subdirectory
+   // 👈 Add this if deploying to a subdirectory
   plugins: [react()],
   build: {
     outDir: "dist",
