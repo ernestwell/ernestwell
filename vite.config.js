52aa1679e82a85784030bbe036061bce422f92e2
ernestwell <ernestwellbusiness@gmail.com>
2025-05-02 18:55:27 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: '/ernestwell1/client/',
   plugins: [react()],
   build: {
     outDir: "dist", // Ensure Vercel picks up this folder
